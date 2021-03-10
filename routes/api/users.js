74ed3906660e84da994f2b365a61c2a2d6130080
Keval Charla <kevalcharla@gmail.com>
2021-03-11 02:30:03 +0530

    Some bug fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,30 @@ const cloudinary = require("../../helpers/Cloudinary");
 
 const router = express.Router();
 
+router.put("/:userId/follow", async (req, res, next) => {
+    try {
+        var userId = req.params.userId
+
+        var user = await User.findById(userId);
+
+        if (!user) {
+            return res.sendStatus(404);
+        }
+
+        var isFollowing = user.followers && user.followers.includes(req.session.user._id);
+        var option = isFollowing ? "$pull" : "$addToSet";
+
+        req.session.user = await User.findByIdAndUpdate(req.session.user._id, { [option]: { following: userId } }, { new: true });
+
+        await User.findByIdAndUpdate(userId, { [option]: { followers: req.session.user._id } }, { new: true });
+
+        res.status(200).send(req.session.user);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(400);
+    }
+})
+
 router.post("/profilePicture", upload.single('croppedImage'), async (req, res, next) => {
     try {
         if (!req.file) {
